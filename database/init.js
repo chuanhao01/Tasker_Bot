f27b81cd494e947b414a3f0bc96876e40ac01218
chuanhao01 <chuanhaolim@gmail.com>
2020-04-25 15:40:17 +0800

    adding db creation

--- database/init.js
+++ database/init.js
@@ -35,6 +35,46 @@ CREATE TABLE IF NOT EXISTS TASKSWITHOUTDEADLINE(
     duration INTEGER NOT NULL,
     PRIMARY KEY(taskUID)
 );
-
 `;
+
+async function setUpDB(){
+    const client = await pool.connect()
+    try {
+        await client.query('BEGIN')
+        const queryText = 'INSERT INTO users(name) VALUES($1) RETURNING id'
+        const res = await client.query(queryText, ['brianc'])
+        const insertPhotoText = 'INSERT INTO photos(user_id, photo_url) VALUES ($1, $2)'
+        const insertPhotoValues = [res.rows[0].id, 's3.bucket.foo']
+        await client.query(insertPhotoText, insertPhotoValues)
+        await client.query('COMMIT')
+    }
+    catch (e) {
+        await client.query('ROLLBACK')
+        throw e;
+    }
+    finally {
+        // At the end remember to release client cause we using low level stuff
+        client.release();
+    }
+}
+
+async () => {
+  // note: we don't try/catch this because if connecting throws an exception
+  // we don't need to dispose of the client (it will be undefined)
+  const client = await pool.connect()
+  try {
+    await client.query('BEGIN')
+    const queryText = 'INSERT INTO users(name) VALUES($1) RETURNING id'
+    const res = await client.query(queryText, ['brianc'])
+    const insertPhotoText = 'INSERT INTO photos(user_id, photo_url) VALUES ($1, $2)'
+    const insertPhotoValues = [res.rows[0].id, 's3.bucket.foo']
+    await client.query(insertPhotoText, insertPhotoValues)
+    await client.query('COMMIT')
+  } catch (e) {
+    await client.query('ROLLBACK')
+    throw e
+  } finally {
+    client.release()
+  }
+})().catch(e => console.error(e.stack))
 
