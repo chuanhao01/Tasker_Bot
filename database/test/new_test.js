d85c0fa4d4bc74198ba59559cf22bd414d75fd54
chuanhao01 <chuanhaolim@gmail.com>
2020-04-29 23:13:47 +0800

    Test how to test with mocha, template saved here

---
+++ database/test/new_test.js
@@ -0,0 +1,7 @@
+const assert = require('chai').assert;
+
+describe('hello this is new test', function(){
+    it('this should fail', function(){
+        assert.isTrue(true, 'This should be true');
+    });
+});
