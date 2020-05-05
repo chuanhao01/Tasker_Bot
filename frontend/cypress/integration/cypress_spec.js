1feabf63796291437b222a99398cbd9149091538
SherisseTJW <tanjingwensherisse@gmail.com>
2020-05-05 15:02:49 +0800

    Added files

--- frontend/cypress/integration/cypress_spec.js
+++
@@ -1,7 +0,0 @@
-describe("Cypress unit testing", function () {
-    it("is a test", function () {
-        expect(true).to.equal(true)
-
-        cy.visit("http://192.168.1.141:8080")
-    })
-})
--- frontend/files/functions.js
+++
@@ -1,3 +0,0 @@
-function addNewTask() {
-    console.log("")
-}
