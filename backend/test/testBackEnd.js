803115efdb8c2312bbd4569db24e1d1b045a0f7a
chuanhao01 <chuanhaolim@gmail.com>
2020-05-09 01:39:21 +0800

    Adding test of tests for backend

---
+++ backend/test/testBackEnd.js
@@ -0,0 +1,38 @@
+/**
+ * @fileoverview 
+ * Mocha test for testing and checking normal db functions
+ * Meant to run while db is in production already
+ * 
+ * @author Lim Chuan Hao
+ * 
+ * @requires NPM:chai
+ * @requires NPM:chai-http
+ * @requires ../app.js
+ * 
+ */
+
+// Importing the npm testing lib
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const assert = require('chai').assert;
+const should = require('chai').should();
+
+// Importing own modules
+const app = require('../app');
+
+
+
+chai.request(app)
+.post('/basic/insert')
+.type('form')
+.send({
+    data: ['hello'],
+})
+.end(function(err, res){
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log(res.status);
+});
