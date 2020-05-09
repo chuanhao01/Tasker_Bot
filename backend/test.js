96368a24d994f6211cd129ce453129a1cce2e00d
chuanhao01 <chuanhaolim@gmail.com>
2020-05-09 21:32:55 +0800

    Making sure the test work again in backend

--- backend/test.js
+++ backend/test.js
@@ -1,4 +1,4 @@
-const db = require('../src/index');
+const db = require('../db/index');
 
 // const arr = ['(11, 11, \'1998-02-01\', \'13:07:00\', 2)', '(21, 21, \'1998-02-02\', \'01:32:00\', 22)'];
 // db.basic.insertTask(arr)
