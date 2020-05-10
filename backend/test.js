e5294d006fd93f8f0f2a0b50e193701f95665339
chuanhao01 <chuanhaolim@gmail.com>
2020-05-10 15:12:03 +0800

    Minor change for test.js

--- backend/test.js
+++ backend/test.js
@@ -1,4 +1,5 @@
-const db = require('../db/index');
+const db = require('./db/index');
+const utils = require('./utils');
 
 // const arr = ['(11, 11, \'1998-02-01\', \'13:07:00\', 2)', '(21, 21, \'1998-02-02\', \'01:32:00\', 22)'];
 // db.basic.insertTask(arr)
@@ -9,20 +10,48 @@ const db = require('../db/index');
 // );
             
 
-const {Pool} = require('pg');
+// const {Pool} = require('pg');
 
-let pool = new Pool({
-    connectionString: process.env.PG_URL,
-    max: 5,
-});
+// let pool = new Pool({
+//     connectionString: process.env.PG_URL,
+//     max: 5,
+// });
 
-pool.query(`
-SELECT * FROM TASKSBASIC
-WHERE taskId IN (11, 21)
-`, function(err, res){
-    if(err){
-        console.log(err);
-        return;
+// pool.query(`
+// SELECT * FROM TASKSBASIC
+// WHERE taskId IN (11, 21)
+// `, function(err, res){
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log(res.rows);
+// });
+
+let tasks = [];
+
+const basic_tasks = {
+    "taskId": 1,
+    "projectId": 11,
+    "dueDate": "1998/02/02",
+    "dueTime": "0132",
+    "duration": 2,
+};
+
+for(let i=0; i<10; i++){
+    let new_task = {};
+    new_task.taskId = basic_tasks.taskId + i;
+    new_task.projectId = basic_tasks.projectId;
+    new_task.dueDate = basic_tasks.dueDate;
+    new_task.dueTime = basic_tasks.dueTime;
+    new_task.duration = basic_tasks.duration;
+    tasks.push(new_task);
+}
+
+console.log(utils.dbParser.basic.bulkInsert(tasks));
+db.basic.insertTask(utils.dbParser.basic.bulkInsert(tasks))
+.then(
+    function(res){
+        console.log(res.rows);
     }
-    console.log(res.rows);
-});
+);
