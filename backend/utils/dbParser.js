ee9dc578b781365f540ce94d63b72c455fbb818f
chuanhao01 <chuanhaolim@gmail.com>
2020-05-10 14:22:37 +0800

    Adding getDataQuery parser to sql, also adding test for the util

--- backend/utils/dbParser.js
+++ backend/utils/dbParser.js
@@ -14,6 +14,84 @@
  * 
  */
 const dbParser = {
+    // The parsers that can be used by both the advanced and basic problem statments api endpoints
+    all: {
+        /**
+         * @function
+         * This parsing function is to parse the req.query from the get data api endpoint, and turn the object into the sql statement for the model call
+         *
+         * @param {Object} query The req.query object from the api request, possible template may look like: 
+         * {
+         *     "projectId": {
+         *         ">": "1"
+         *     },
+         *     "duration": {
+         *         "<=": "10"
+         *     },
+         *     "sortBy": "projectId.asc,taskId.asc",
+         *     "page": "1",
+         *     "pageNum": "1"
+         * }
+         * 
+         * @returns {String} The query string to be passed down to the model to call for the data
+         * 
+         */
+        getDataQueryParams(query){
+            // Deafult sql query to return if no query params are included
+            if(query === {} || Object.keys(query).length === 0){
+                return 'LIMIT 10';
+            }
+            let sqlString = '';
+            /*
+            Adding in the filters where they exists
+            This is the WHERE clause in the select statement
+            */
+            if(query.projectId && query.duration){
+                sqlString += `WHERE \n`;
+                sqlString += `projectId ${Object.keys(query.projectId)[0]} ${Object.values(query.projectId)[0]} \n`;
+                sqlString += `AND \n`;
+                sqlString += `duration ${Object.keys(query.duration)[0]} ${Object.values(query.duration)[0]} \n`;
+            }
+            else if(query.projectId){
+                sqlString += `WHERE \n`;
+                sqlString += `projectId ${Object.keys(query.projectId)[0]} ${Object.values(query.projectId)[0]} \n`;
+            }
+            else if(query.duration){
+                sqlString += `WHERE \n`;
+                sqlString += `duration ${Object.keys(query.duration)[0]} ${Object.values(query.duration)[0]} \n`;
+            }
+            /*
+            Now we will be adding sortBy order now, 
+            Which is the ORDER BY clause in the select statement
+            */
+           if(query.sortBy){
+               sqlString += `ORDER BY \n`;
+                const sortOrders = query.sortBy.split(',');
+                for(let i=0; i<sortOrders.length; i++){
+                    const values = sortOrders[i].split('.');
+                    if(i < sortOrders.length - 1){
+                        sqlString += `${values[0]} ${values[1]}, `;
+                    }
+                    else{
+                        sqlString += `${values[0]} ${values[1]}\n`;
+                    }
+                }
+           }
+           /*
+           Adding pagination based on the query params
+           This includes both the page and pageNum
+           */
+           // This is special as if there is no pageNum given, go back on default 10
+           if(!('pageNum' in query)){
+               query.pageNum = 10;
+           }
+           sqlString += `LIMIT ${query.pageNum} `;
+           if(query.page){
+               sqlString += `OFFSET ${(query.page - 1) * query.pageNum}`;
+           }
+           return sqlString;
+        }
+    },
     // The parser for the basic problems
     basic: {
         // Parsing for the basic db models
@@ -49,7 +127,7 @@ const dbParser = {
                 new_tasks.push(`(${task.taskId}, ${task.projectId}, '${task.dueDate.replace(new RegExp('/', 'g'), '-')}', '${new_time}', ${task.duration})`);
             }
             return new_tasks;
-        }
+        },
     }
 };
 
