11cd2ca42c517ebeb263e7c0086b934f6e60d23e
SherisseTJW <tanjingwensherisse@gmail.com>
2020-05-20 13:08:46 +0800

    Cleaning up index.html

--- frontend/files/script.js
+++ frontend/files/script.js
@@ -10,7 +10,10 @@
  var pageNumType = 10;
  var sortType = '';
 
-
+ 
+/**
+ * @function Implementing filter functionality in the basic data viewer table
+ */
  function filter() {
     let filterAttribute = $('#filterAttribute option:selected').text();
     let filterOperation = $('#filterOperation option:selected').text();
@@ -78,7 +81,12 @@
  };
 
 
- function sort(eventTarget) {
+/**
+ * @function Implementing sorting functionality in the basic data viewer table
+ * 
+ * @param {object} eventTarget 
+ */
+function sort(eventTarget) {
     // Obtain whether the column is already sorted in ascending order or descending order
     var currentSort = eventTarget.className.split(' ')[1];
 
@@ -97,9 +105,14 @@
     };
     
     obtainData("", "", `sortBy=${sortType}`, `${currentPage}`, `pageNum=${pageNumType}`);
- };
+};
 
 
+/**
+ * @function Implementing sorting functionality in the basic data viewer table
+ * 
+ * @param {string} pageLinkId 
+ */
 function pagination(pageLinkId) {
     var paginationType;
     paginationType = pageLinkId.split('_')[1];
@@ -129,6 +142,11 @@ function pagination(pageLinkId) {
 };
 
 
+/**
+ * @function Implementing sorting functionality in the basic data viewer table
+ * 
+ * @param {int} pageNumType 
+ */
 function pageNum(pageNumType) {
     obtainData("", "", "", `page=${currentPage}`, `pageNum=${pageNumType}`);
     obtainTotalPage("", "", "", `page=${currentPage}`, `pageNum=${pageNumType}`);
