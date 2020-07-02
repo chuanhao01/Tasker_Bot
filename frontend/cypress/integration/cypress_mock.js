c3f37b166e08b14307032a3547a445c7d1a98455
SherisseTJW <tanjingwensherisse@gmail.com>
2020-07-02 10:46:45 +0800

    Fixed error with stubbing request from frontend in Cypress test

--- frontend/cypress/integration/cypress_mock.js
+++ frontend/cypress/integration/cypress_mock.js
@@ -17,40 +17,6 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 
 
 
-describe("Load the basic data and result viewer", () => {
-    it("Checks whether the webpages loads properly", () => {
-        cy.visit("http://127.0.0.1:8080/index.html");
-
-        // Check that the pop-up form is automatically hidden
-        cy.get('#insert_editModal').should('not.be.visible');
-
-        // Click the add new btn to reveal the pop-up form
-        cy.get('#addNewBtn').click();
-
-        // Check that the pop-up form is now visible after clicking and that the close button works properly
-        cy.get('#insert_editModal').should('be.visible');
-        cy.get('#addNewTask_closeBtn').click()
-        cy.get('#insert_editModal').should('not.be.visible');
-
-
-        // Navigate to the result viewer page
-        cy.get('#resultNav').should('have.attr', 'href')
-                            .then((href) => {
-                                cy.visit(`http://127.0.0.1:8080/${href}`);
-                            });
-
-        // Check that the table and chart are visible
-        cy.get('#resultTable').should('be.visible');
-        cy.get('#resultChart').should('be.visible');
-
-
-        // Return to the basic data viewer (homepage -> index.html)
-        cy.visit("http://127.0.0.1:8080/index.html");
-        cy.wait(2000);
-    });
-});
-
-
 describe("Test ajax call for basic data viewer", () => {
     it("Perform an ajax call and respond with mock data", () => {
         // Enabling response stubbing
@@ -58,15 +24,11 @@ describe("Test ajax call for basic data viewer", () => {
 
         // Reroute the request and respond with a pre-determined mock data ( ../fixtures/data.json )
         cy.fixture('data').then((data) => {
-            cy.route({
-                method: 'GET',
-                url: '/basic/*',
-                data  
-            }).as('basicData')
+            cy.route('GET', '/basic/data?*', data).as('basicData')
         });
 
         // Use a random functionality that will perform an ajax call
-        cy.get('#page_2').click()
+        cy.visit("http://127.0.0.1:8080/index.html");
 
         // Wait for the backend routing to finish and provide assertions for the expected data
         cy.wait('@basicData').its('responseBody')
@@ -74,4 +36,5 @@ describe("Test ajax call for basic data viewer", () => {
           .and('not.empty')
     });
 });
+
 
