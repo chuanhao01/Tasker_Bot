f2a438df11d439c3ec1492cf283d3debcaab25c5
SherisseTJW <tanjingwensherisse@gmail.com>
2020-07-02 10:48:47 +0800

    Added general cypress test for advanceData viewer with stubbed request

--- frontend/cypress/integration/cypress_mock.js
+++ frontend/cypress/integration/cypress_mock.js
@@ -38,3 +38,22 @@ describe("Test ajax call for basic data viewer", () => {
 });
 
 
+describe("Test ajax call for basic data viewer", () => {
+    it("Perform an ajax call and respond with mock data", () => {
+        // Enabling response stubbing
+        cy.server();
+
+        // Reroute the request and respond with a pre-determined mock data ( ../fixtures/data.json )
+        cy.fixture('data').then((data) => {
+            cy.route('GET', '/advance/data?*', data).as('advanceData')
+        });
+
+        // Use a random functionality that will perform an ajax call
+        cy.visit("http://127.0.0.1:8080/advanced_data.html");
+
+        // Wait for the backend routing to finish and provide assertions for the expected data
+        cy.wait('@advanceData').its('responseBody')
+          .should('be.an', 'object')
+          .and('not.empty')
+    });
+});
