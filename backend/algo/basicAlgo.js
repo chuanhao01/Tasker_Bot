b29867eb26178bc116b8f1161b70fa46a9772e49
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2020-07-05 14:05:41 +0800

    Started working on the algo for basic result

---
+++ backend/algo/basicAlgo.js
@@ -0,0 +1,18 @@
+/**
+ * @fileoverview 
+ * Contains the algo to calculate the basic results
+ * 
+ * @author Lim Chuan Hao
+ * 
+ * @requires moment
+ * 
+ */
+
+// Importing libs needed
+const moment = require('moment');
+
+const basicAlgo = {
+
+};
+
+module.exports = basicAlgo;
