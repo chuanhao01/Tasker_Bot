80f4c0016fb9cbcbc8d6385b0d88332e94dfad15
SherisseTJW <tanjingwensherisse@gmail.com>
2020-07-09 11:34:14 +0800

    Added a Cypress command that performs basic GET testing with mock backend call

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -1,25 +1,17 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add("stubBackend", (url, basicAdvance, dataResult) => {
+    // Enable response stubbing
+    cy.server()
+
+    // Reroute request and respond with pre-determined mock data (../fixtures/data.json)
+    cy.fixture('data').then((data) => {
+        cy.route('GET', `/${basicAdvance}/${dataResult}?*`, data).as(`${basicAdvance}_${dataResult}`);
+    });
+
+    // Visit a relevant page that will call the rerouted backend
+    cy.visit(url);
+
+    // Ensure that a response has been provided from the rerouted backend and provide assertions for said response
+    cy.wait(`@${basicAdvance}_${dataResult}`).its('responseBody')
+      .should('be.an', 'object')
+      .and('not.empty');
+})
