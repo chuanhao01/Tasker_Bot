616a31e2fc8643b4d99827b38619706c2c17ebdc
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2020-07-12 21:50:05 +0800

    change util parse so that int test pass 1

--- backend/utils/dataParser.js
+++ backend/utils/dataParser.js
@@ -13,7 +13,21 @@
  * 
  */
 const dataParser = {
-    // Parsers to be used for the basic problem
+    // Parent/global parsers to be used, mainly to parse to custom data types
+    all: {
+        /**
+         * @function
+         * This function parses a number into the HOURS data type, meant to be sent back as a response
+         *
+         * @param {Number} hours the hours in number format
+         * 
+         * @returns {String} String representation of the hours rounded off to 3dp
+         * 
+         */
+        roundHours(hours){
+            return (Math.round((hours + Number.EPSILON) * 1000) / 1000).toString(10);
+        }
+    },
 
     // Data parsers specifically for the basic problem statement
     basic: {
@@ -40,20 +54,17 @@ const dataParser = {
             // Defining helper function to add padding
             for(let i=0; i<data.length; i++){
                 // Getting the values
-                let dueDate = data[i].duedate.format('YYYY/MM/DD'),
-                dueTime = `${data[i].duetime}`;
-                let taskId = data[i].taskid,
-                projectId = data[i].projectid;
+                let dueDate = data[i].duedate.format('YYYY/MM/DD');
+                let dueTime = `${data[i].duetime}`;
+                let duration = data[i].duration;
                 // Formatting and parsing them
                 dueTime = dueTime.substring(0, 5);
                 dueTime = dueTime.replace(':', '');
-                taskId = parseInt(taskId);
-                projectId = parseInt(projectId);
+                duration = dataParser.all.roundHours(duration);
                 // Setting values back
                 data[i].duedate = dueDate;
                 data[i].duetime = dueTime;
-                data[i].taskid = taskId;
-                data[i].projectid = projectId;
+                data[i].duration = duration;
             }
             return data;
         },
@@ -68,14 +79,27 @@ const dataParser = {
          */
         getResults(result){
             for(let task of result.data){ 
-                task['lateness'] = (Math.round((task['lateness'] + Number.EPSILON) * 1000) / 1000).toString(10);
+                task['lateness'] = dataParser.all.roundHours(task['lateness']);
             }
-            result.totalLateness = (Math.round((result.totalLateness + Number.EPSILON) * 1000) / 1000).toString(10);
+            result.totalLateness = dataParser.all.roundHours(result.totalLateness);
             const parsedResult = {
                 'result': result.data,
                 'totalLateness': result.totalLateness
             };
             return parsedResult;
+        }
+    },
+    advanced: {
+        /**
+         * @function
+         *
+         * @param 
+         * 
+         * @returns 
+         * 
+         */
+        getData(pgRes){
+
         }
     }
 };
