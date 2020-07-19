a0bebb43a166f0e7c24977f9de9f4a8b640098ec
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2020-07-19 15:18:23 +0800

    done with the rec advanced algo

--- backend/algo/advancedAlgo.js
+++ backend/algo/advancedAlgo.js
@@ -8,8 +8,37 @@
 
 const advancedAlgo = {
     splitEqual:{
+        /**
+         * @function
+         * Function to calculate the result for the advanced problem
+         * 
+         * @param {Array} tasks Array of tasks for the advanced problem
+         * 
+         * @returns {Array} An array of the tasks for the first and second person
+         * [[tasks for first person], [tasks for the second person]]
+         * 
+         */
         calculateResults(tasks){
-
+            // Calculate sum of duration to find split and find out if there are decimals
+            let sum = 0;
+            let includeFloats = false;
+            for(let task of tasks){
+                if(!Number.isSafeInteger(task.duration)){
+                    includeFloats = true;
+                }
+                sum += task.duration;
+            }
+            // Get the task allocation based on algo
+            let taskAllocation;
+            if(includeFloats){
+                taskAllocation = this.customKnapsackRec(task, sum/2);
+            }
+            // Tasks for each person to be returned
+            let personTasks = [[], []];
+            for(let i=0; i<taskAllocation.length; i++){
+                personTasks[taskAllocation[i]].push(tasks[i]);
+            }
+            return personTasks;
         },
         /**
          * @function
@@ -24,40 +53,40 @@ const advancedAlgo = {
          * @returns {Array} An array contain 0/1 for which person should take the task
          * 
          */
-        customKnapsack(tasks, target){
+        customKnapsackRec(tasks, target){
             // Creating the 0/1 array to assign tasks to the person
-            let personTasks = [];
+            let taskAllocation = [];
             for(let task of tasks){
-                personTasks.push(-1);
+                taskAllocation.push(-1);
             }
             // Recursive function call to solve
             function rec(n, c){
                 // Base case
                 if(n === 0 || c === 0){
                     if(n > 0){
-                        personTasks[n-1] = 0;
+                        taskAllocation[n-1] = 0;
                     }
                     return 0;
                 }
                 // Rec, pushing factor
                 if(tasks[n-1] < c){
-                    personTasks[n-1] = 0;
+                    taskAllocation[n-1] = 0;
                     return rec(n-1, c);
                 }
                 else{
                     const takeItem = tasks[n-1] + rec(n-1, c-tasks[n-1]);
                     const dontTakeItem = rec(n-1, c);
                     if(takeItem > dontTakeItem){
-                        personTasks[n-1] = 1;
+                        taskAllocation[n-1] = 1;
                     }
                     else{
-                        personTasks[n-1] = 0;
+                        taskAllocation[n-1] = 0;
                     }
                     return Math.max(takeItem, dontTakeItem);
                 }
             }
             rec(tasks.length, target);
-            return personTasks;
+            return taskAllocation;
         }
     },
 };
