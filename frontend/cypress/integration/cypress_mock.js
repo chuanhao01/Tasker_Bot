05bbd5c0be0067314687ac7cdb7726e056002359
SherisseTJW <tanjingwensherisse@gmail.com>
2020-08-07 16:53:30 +0800

    Seperated basic and advance dataViewer into 2 test suites

--- frontend/cypress/integration/cypress_mock.js
+++ frontend/cypress/integration/cypress_mock.js
@@ -19,73 +19,83 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 const baseUrl = "http://127.0.0.1:8080";
 
 
-describe("Integration testing for data viewer - basic & advance", () => {
+describe("Integration testing for data viewer - basic", () => {
+    var url = `${baseUrl}/index.html`
     var dataResult_type = 'data';
 
     it("Perform an ajax call with default params and responds with pre-determined mock data", () => {
-        cy.stubBackend(`${baseUrl}/index.html`, "basic", dataResult_type, 'basicData');
-        cy.stubBackend(`${baseUrl}/advanced_data.html`, "advance", dataResult_type, 'advanceData');
+        cy.stubBackend(url, "basic", dataResult_type, 'basicData');
     });
 
     it("Uses a cypress command to ensure that the filter feature is able to provide the accurate url - basic; projectId", () => {
         /* ProjectId */
 
         // Equal to
-        cy.checkFilterFeature(`${baseUrl}/index.html`, "basic", dataResult_type, "projectId", "Equal to", "19999999");
+        cy.checkFilterFeature(url, "basic", dataResult_type, "projectId", "Equal to", "19999999");
 
         // Less than
-        cy.checkFilterFeature(`${baseUrl}/index.html`, "basic", dataResult_type, "projectId", "Less than", "0");
+        cy.checkFilterFeature(url, "basic", dataResult_type, "projectId", "Less than", "0");
 
         // Greater than
-        cy.checkFilterFeature(`${baseUrl}/index.html`, "basic", dataResult_type, "projectId", "Greater than", "10");
+        cy.checkFilterFeature(url, "basic", dataResult_type, "projectId", "Greater than", "10");
     });
 
     it("Uses a cypress command to ensure that the filter feature is able to provide the accurate url - basic; duration", () => {
          /* duration */
 
         // Equal to
-        cy.checkFilterFeature(`${baseUrl}/index.html`, "basic", dataResult_type, "duration", "Equal to", "19999999");
+        cy.checkFilterFeature(url, "basic", dataResult_type, "duration", "Equal to", "19999999");
 
         // Less than
-        cy.checkFilterFeature(`${baseUrl}/index.html`, "basic", dataResult_type, "duration", "Less than", "0");
+        cy.checkFilterFeature(url, "basic", dataResult_type, "duration", "Less than", "0");
 
         // Greater than
-        cy.checkFilterFeature(`${baseUrl}/index.html`, "basic", dataResult_type, "duration", "Greater than", "10");
+        cy.checkFilterFeature(url, "basic", dataResult_type, "duration", "Greater than", "10");
     });
 
     it("Uses a cypress command to ensure that the pageNum feature is able to provide the accurate url - basic", () => {
         var pageNum = 5;
-        cy.checkPageNumFeature(`${baseUrl}/index.html`, "basic", dataResult_type, pageNum);
-    })
+        cy.checkPageNumFeature(url, "basic", dataResult_type, pageNum);
+    });
+});
+
+
+describe("Integration testing for data viewer - advance", () => {
+    var url = `${baseUrl}/advanced_data.html`;
+    var dataResult_type = 'data';
+
+    it("Perform an ajax call with default params and responds with pre-determined mock data", () => {
+        cy.stubBackend(url, "advance", dataResult_type, 'advanceData');
+    });
 
     it("Uses a cypress command to ensure that the filter feature is able to provide the accurate url - advance; projectId", () => {
         /* ProjectId */
 
         // Equal to
-        cy.checkFilterFeature(`${baseUrl}/advanced_data.html`, "advance", dataResult_type, "projectId", "Equal to", "19999999");
+        cy.checkFilterFeature(url, "advance", dataResult_type, "projectId", "Equal to", "19999999");
 
         // Less than
-        cy.checkFilterFeature(`${baseUrl}/advanced_data.html`, "advance", dataResult_type, "projectId", "Less than", "0");
+        cy.checkFilterFeature(url, "advance", dataResult_type, "projectId", "Less than", "0");
 
         // Greater than
-        cy.checkFilterFeature(`${baseUrl}/advanced_data.html`, "advance", dataResult_type, "projectId", "Greater than", "10");
+        cy.checkFilterFeature(url, "advance", dataResult_type, "projectId", "Greater than", "10");
     });
 
     it("Uses a cypress command to ensure that the filter feature is able to provide the accurate url - advance; duration", () => {
          /* duration */
 
         // Equal to
-        cy.checkFilterFeature(`${baseUrl}/advanced_data.html`, "advance", dataResult_type, "duration", "Equal to", "19999999");
+        cy.checkFilterFeature(url, "advance", dataResult_type, "duration", "Equal to", "19999999");
 
         // Less than
-        cy.checkFilterFeature(`${baseUrl}/advanced_data.html`, "advance", dataResult_type, "duration", "Less than", "0");
+        cy.checkFilterFeature(url, "advance", dataResult_type, "duration", "Less than", "0");
 
         // Greater than
-        cy.checkFilterFeature(`${baseUrl}/advanced_data.html`, "advance", dataResult_type, "duration", "Greater than", "10");
+        cy.checkFilterFeature(url, "advance", dataResult_type, "duration", "Greater than", "10");
     });
 
     it("Uses a cypress command to ensure that the pageNum feature is able to provide the accurate url - advance", () => {
         var pageNum = 5;
-        cy.checkPageNumFeature(`${baseUrl}/advanced_data.html`, "advance", dataResult_type, pageNum);
+        cy.checkPageNumFeature(url, "advance", dataResult_type, pageNum);
     })
 });
